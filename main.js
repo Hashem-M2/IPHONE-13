5422d14be2ac0ed57b2ec7402a1de75ead9abb7a
hashem mohamed <hm.cassem@gmail.com>
2023-04-01 00:57:31 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+function change(phone) {
+    img.src = phone;
+};
+function bkg(ground) {
+    container.style.background = ground;
+}
